bf9a186ad5a56f173fdfb5ffdf71ff856d7f2042
richard chen <clq_web@126.com>
2015-07-11 11:23:00 +0800

    test

---
+++ test/fis-conf.js
@@ -0,0 +1,11 @@
+// 使用fis-parser-velocity直接编译html文件
+fis.match('*.html', {
+        parser: fis.plugin('velocity', {
+            encoding: 'utf-8'
+        })
+    })
+    // 加添scss编译
+    .match('*.scss', {
+        rExt: '.css',
+        parser: fis.plugin('sass')
+    })
