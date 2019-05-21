11979bce622c18203e32710f1b2ed4f8ad60bb9b
陈礼强 <cdchenliqiang@jd.com>
2019-05-21 16:56:17 +0800

    add try catch

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = function(content, file, settings) {
+  try {
     return require('./lib')(content, file, settings).renderTpl();
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 };
